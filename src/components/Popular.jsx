5f81645b864aeefebc8bb446470a92afc1649c7e
Chris Mbah <mbahchris46@gmail.com>
2023-08-20 16:58:47 +0100

    Minor changes

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { styled } from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 export default function Popular() {
   const [popular, setPopular] = useState([]);
@@ -9,7 +12,7 @@ export default function Popular() {
 
   const getPopular = async () => {
     const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=cfe9b37e897f48eea3603d73556d9ed6&number=9`
+      `https://api.spoonacular.com/recipes/random?apiKey=cc743949391a4028a81626d56eee385a&number=12`
     );
     const data = await api.json();
     setPopular(data.recipes);
@@ -17,16 +20,55 @@ export default function Popular() {
   };
   return (
     <>
-      {popular.map((recipe) => {
-        return (
-          <div key={recipe.id}>
-            <h2>{recipe.title}</h2>
-            <p>{recipe.summary}</p>
-          </div>
-        );
-      })}
+      <Wrapper>
+        <h3>Popular Picks</h3>
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
+          {popular.map((recipe) => {
+            return (
+              <SplideSlide>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
     </>
   );
 }
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  p {
+    position: absolute;
+    z-index: 10;
+  }
+`;
+
 //? =${process.env.REACT_APP_API_KEY}
 //! cfe9b37e897f48eea3603d73556d9ed6
+//* cc743949391a4028a81626d56eee385a

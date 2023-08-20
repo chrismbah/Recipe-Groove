91181d12f9fc07165e5799f38de88155294d4c0e
Chris Mbah <mbahchris46@gmail.com>
2023-08-20 13:12:37 +0100

    Installed necessary dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import Pages from './pages/Pages'
+import { useEffect } from 'react';
 
 export default function App() {
+  useEffect(() => {
+    window.process = {
+      ...window.process,
+    };
+  }, []);
   return (
-    <div>App</div>
+    <div className='App'>
+      <Pages />
+    </div>
   )
 }

6ff5ca78b61e3f147b9be2d9604af8b00cd9e196
Chris Mbah <mbahchris46@gmail.com>
2023-08-20 20:22:45 +0100

    Implemented fetch API for Veggie section

--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
+import { styled } from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 export default function Veggie() {
   return (
-    <div>Veggie</div>
-  )
+    <>
+      <Wrapper>
+        <h3>Popular Picks</h3>
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
+          {popular.map((recipe, id) => {
+            return (
+              <SplideSlide>
+                <Card key={id}>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
+    </>
+  );
 }
